c77cbbbbc9374129b2ba6e7864568a6feb08d23d
tt67wq <tt67wq@aliyun.com>
2025-05-18 18:56:40 +0800

    Revamp Makefile with clearer targets and update ex_doc

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule RedisPool.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:styler, "~> 0.11", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.32", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.38", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
