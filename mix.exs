0bb039ac472536aa7c50b732ad864d68b1800209
wanqiang <wanqiang@shizhuang-inc.com>
2025-08-05 10:23:41 +0800

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RedisPool.MixProject do
   use Mix.Project
 
   @name "redis_pool_xyz"
-  @version "0.3.0"
+  @version "0.3.1"
   @repo_url "https://github.com/tt67wq/redis_pool"
   @description "A pool wrapper for redix using NimblePool"
 
